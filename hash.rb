ea41ce326dbd3e7f45049560dcce501527e325a2
Wes Dixon <jdixon3@ycp.edu>
2020-06-15 22:02:48 -0700

    ruby book exercises 1

---
+++ hash.rb
@@ -0,0 +1,8 @@
+# write a program that uses a hash to store a list of movie titles with the year they came out.
+
+movies = {:terminator => 1996,
+          :jaws => 1976,
+          :matrix => 1999
+        }
+
+        movies.each { |k, v| puts v }
