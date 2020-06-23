5e4375b6e828b55af0a4c1ab447f588de3b1fdfd
Wes Dixon <jdixon3@ycp.edu>
2020-06-22 20:47:09 -0700

    ruby book exercises cont..

---
+++ case_exercises.rb
@@ -0,0 +1,13 @@
+puts "Give me a number between 0 andf 100."
+number = gets.chomp.to_i
+
+if number <= 50
+  puts "Your number is between 0 and 50!"
+else 
+  puts "Your number is between 51 and 100!"
+end 
+
+
+
+
+
---
+++ case_statement.rb
@@ -0,0 +1,14 @@
+# Case_statement.rb - rafactored
+
+a = 5
+
+answer = case a 
+when 5
+  "a is 5"
+when 6
+  "a is 6"
+else
+  "a is neither"
+end 
+
+puts answer
